9794e19651d6965e8a1a2ddcf878f79c2175102f
ray.rodz.88@gmail.com Rodriguez <ray.rodz.88@gmail.com>
2018-05-29 19:43:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var katzDeli=[];
-
-function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name);
-  return "Welcome, "+name+". You are number " +(katzDeliLine.indexOf(name)+ 1)+" in line."}
+var deliCounter=0;
+function takeANumber(katzDeliLine){
+  deliCounter++;
+  katzDeliLine.push(deliCounter);
+  return `Welcome you are number ${deliCounter}`
+}
   
  function nowServing(katzDeliLine) {
 	if (katzDeliLine.length === 0) {
@@ -12,14 +14,14 @@ function takeANumber(katzDeliLine, name){
 	}
 }
 
-function currentLine(line){
-  if(line.length === 0) {
+function currentLine(katzDeliLine){
+  if(katzDeliLine.length === 0) {
     return "The line is currently empty.";
   }
   var lineNamesandNumbers = [];
   
-  for(var i=0; i<line.length; i++) {
-    lineNamesandNumbers.push(i+1 + ". "+ line[i]);
+  for(var i=0; i<katzDeliLine.length; i++) {
+    lineNamesandNumbers.push(i+1 + ". "+ katzDeliLine[i]);
   }
   return "The line is currently: " + lineNamesandNumbers.join(', ');
 }
